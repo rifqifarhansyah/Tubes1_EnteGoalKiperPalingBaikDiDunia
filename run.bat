12955f15641ee9a4242144ecdbc25603547cf9da
SaddamAnnais <saddamannaiss@gmail.com>
2023-02-08 22:23:37 +0700

    fix: enums, objecttype, and main

---
+++ run.bat
@@ -0,0 +1,35 @@
+@echo off
+
+cd ./src/JavaBot/
+
+start mvn package
+timeout /t 3
+
+:: Game Runner
+cd ../../runner-publish/
+start "" dotnet GameRunner.dll
+
+:: Game Engine
+cd ../engine-publish/
+timeout /t 1
+start "" dotnet Engine.dll
+
+:: Game Logger
+cd ../logger-publish/
+timeout /t 1
+start "" dotnet Logger.dll
+
+:: Bots
+cd ../reference-bot-publish/
+
+timeout /t 1
+start "" dotnet ReferenceBot.dll
+timeout /t 1
+start "" dotnet ReferenceBot.dll
+timeout /t 1
+start "" dotnet ReferenceBot.dll
+timeout /t 1
+
+cd ../src/JavaBot/target/
+start java -jar JavaBot.jar
+pause
