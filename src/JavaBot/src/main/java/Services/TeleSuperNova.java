e2d842d90e9894d3cf34528d4c523d5c47170a4f
SaktiWidyatmaja <saktiwidyatmaja1@gmail.com>
2023-02-10 20:54:08 +0700

    Upload file berisi fungsi untuk teleport dan supernova

---
+++ src/JavaBot/src/main/java/Services/TeleSuperNova.java
@@ -0,0 +1,202 @@
+package Services;
+
+import Enums.*;
+import Models.*;
+import java.lang.Math;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class TeleSuperNova {
+    private GameObject bot;
+    private World world;  
+    private PlayerAction playerAction;
+    private GameState gameState;
+    private int headingTele;
+    private int headingSupernova;
+
+    public TeleSuperNova() {
+        this.playerAction = new PlayerAction();
+        this.gameState = new GameState();
+    }
+
+    public GameObject getBot() {
+        return this.bot;
+    }
+
+    public void setBot(GameObject bot) {
+        this.bot = bot;
+    }
+
+    public PlayerAction getPlayerAction() {
+        return this.playerAction;
+    }
+
+    public void setPlayerAction(PlayerAction playerAction) {
+        this.playerAction = playerAction;
+    }
+
+    public int teleportSerang(PlayerAction playerAction, int firedTeleport) {
+      // Punya return value yang menandakan heading teleporter dan bahwa telah menembakkan teleporter
+      playerAction.action = PlayerActions.FIRETELEPORT;
+      
+      if (!gameState.getGameObjects().isEmpty()) {
+        // Kalau belum pakai radar
+        var playerList = gameState.getGameObjects()
+                .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
+                .sorted(Comparator
+                        .comparing(item -> getDistanceBetween(bot, item)))
+                .collect(Collectors.toList());
+
+        playerAction.heading = getHeadingBetween(playerList.get(1));
+      }
+
+      return playerAction.heading;
+    }
+
+    public int teleportBertahan(PlayerAction playerAction) {
+      // Punya return value yang menandakan heading teleporter dan bahwa telah menembakkan teleporter
+      playerAction.action = PlayerActions.FIRETELEPORT;
+      
+      if (!gameState.getGameObjects().isEmpty()) {
+        // Kalau belum pakai radar
+        var playerList = gameState.getGameObjects()
+                .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
+                .sorted(Comparator
+                        .comparing(item -> getDistanceBetween(bot, item)))
+                .collect(Collectors.toList());
+
+        // Pakai index 1 untuk cari pemain lain lain terdekat
+        playerAction.heading = (getHeadingBetween(playerList.get(1)) + 90) % 360; // Sementara
+      }
+
+      return playerAction.heading+2160;
+    }
+    
+
+    public int checkTeleportSerang (PlayerAction playerAction, int headingTele) {
+      var teleporterList = gameState.getGameObjects()
+        .stream().filter(item -> item.getGameObjectType() == ObjectTypes.TELEPORTER && 
+          item.getCurrentHeading() == headingTele) // Sementara bandingin heading teleporter
+        .collect(Collectors.toList());
+
+      var enemyList = gameState.getGameObjects()
+        .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
+        .sorted(Comparator
+              .comparing(item -> getDistanceBetween(teleporterList.get(0), item)))
+        .collect(Collectors.toList());
+
+
+      if (!teleporterList.isEmpty()) { // Jika teleporter masih ada dalam permainan
+        var teleporterDistance = getDistanceBetween(teleporterList.get(0), enemyList.get(1));       
+        if (teleporterDistance <= bot.getSize() + 10) { // Jika radius tambah 10 kurang dari jarak teleporter ke musuh terdekat
+          playerAction.action = PlayerActions.TELEPORT;
+          return -1;
+        } 
+
+        // Return heading awal, tunggu tick selanjutnya
+        // jika pada tick2 selanjutnya teleporter melebihi boundary maka otomatis hilang
+        return headingTele; 
+      } else {
+        return -1;
+      }
+    }
+
+    public int checkTeleportBertahan (PlayerAction playerAction, int headingTele) {
+      var teleporterList = gameState.getGameObjects()
+        .stream().filter(item -> item.getGameObjectType() == ObjectTypes.TELEPORTER && 
+          item.getCurrentHeading() == headingTele) // Sementara bandingin heading teleporter
+        .sorted(Comparator
+                .comparing(item -> getDistanceBetween(bot, item)))
+        .collect(Collectors.toList());
+
+      if (!teleporterList.isEmpty()) { // Check apakah teleporter masih ada di lapangan
+        if (headingTele > 720) {
+          var nextX = teleporterList.get(0).getPosition().getX() + 20*Math.cos(headingTele % 360); // Koor X pada tick selanjutnya
+          var nextY = teleporterList.get(0).getPosition().getY() + 20*Math.sin(headingTele % 360); // Koor Y pada tick selanjutnya
+          var nextPosition = Math.sqrt(Math.pow(nextX,2) + Math.pow(nextY,2)); // posisi teleporter pada tick selanjutnya   
+          
+          if (nextPosition > world.getRadius()) { // Jika pada tick selanjutnya teleporter keluar border, teleport sekarang saja
+            playerAction.action = PlayerActions.TELEPORT;
+            return -1;
+          }
+
+          return headingTele - 360;  
+        } else {
+          playerAction.action = PlayerActions.TELEPORT;
+          return -1;
+        }
+      } else {
+        return -1;
+      }
+    }
+
+    public int tembakSuperNova(PlayerAction playerAction) {
+      // Punya return value yang menandakan bahwa telah menembakkan supernova
+
+      return 1;
+    }
+
+    public void computeNextPlayerAction(PlayerAction playerAction) {
+        playerAction.action = PlayerActions.FORWARD;
+        playerAction.heading = new Random().nextInt(360);
+
+        // Kalau mau pakai teleport dan supernova nanti pakai kondisional di luar fungsi 
+        // buat tracking apakah pernah nembak teleporter atau supernova
+        // if (headingTele != -1) {
+              // if (headingTele > 360) {
+              //    headingTele = checkTeleportBertahan();
+              // } else {
+                    // headingTele = checkTeleportSerang();
+              // }  
+        // } 
+
+        // if (headingSupernova != -1) {
+              // headingSupernova = checkDetonateSuperNova();
+        // } 
+
+        // else baru cek di bawah
+        if (!gameState.getGameObjects().isEmpty()) {
+            var foodList = gameState.getGameObjects()
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
+                    .sorted(Comparator
+                            .comparing(item -> getDistanceBetween(bot, item)))
+                    .collect(Collectors.toList());
+
+            playerAction.heading = getHeadingBetween(foodList.get(0));
+        }
+
+        this.playerAction = playerAction;
+    }
+
+    public GameState getGameState() {
+        return this.gameState;
+    }
+
+    public void setGameState(GameState gameState) {
+        this.gameState = gameState;
+        updateSelfState();
+    }
+
+    private void updateSelfState() {
+        Optional<GameObject> optionalBot = gameState.getPlayerGameObjects().stream().filter(gameObject -> gameObject.id.equals(bot.id)).findAny();
+        optionalBot.ifPresent(bot -> this.bot = bot);
+    }
+
+    private double getDistanceBetween(GameObject object1, GameObject object2) {
+        var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
+        var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
+        return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
+    }
+
+    private int getHeadingBetween(GameObject otherObject) {
+        var direction = toDegrees(Math.atan2(otherObject.getPosition().y - bot.getPosition().y,
+                otherObject.getPosition().x - bot.getPosition().x));
+        return (direction + 360) % 360;
+    }
+
+    private int toDegrees(double v) {
+        return (int) (v * (180 / Math.PI));
+    }
+
+}
